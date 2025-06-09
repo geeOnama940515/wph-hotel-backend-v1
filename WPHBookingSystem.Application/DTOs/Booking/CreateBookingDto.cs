49b92c94c3f9af748858e18bed2f3ec7d34037dc
Gregorio Amano III <66071367+geeOnama940515@users.noreply.github.com>
2025-06-09 15:20:29 +0800

    Added DTOs

---
+++ WPHBookingSystem.Application/DTOs/Booking/CreateBookingDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPHBookingSystem.Application.DTOs.Booking
+{
+    public class CreateBookingDto
+    {
+        public Guid UserId { get; set; }
+        public Guid RoomId { get; set; }
+
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public int Guests { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string SpecialRequests { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
+}
---
+++ WPHBookingSystem.Application/DTOs/Booking/UpdateBookingDateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPHBookingSystem.Application.DTOs.Booking
+{
+    public class UpdateBookingDatesDto
+    {
+        public Guid BookingId { get; set; }
+        public DateTime NewCheckIn { get; set; }
+        public DateTime NewCheckOut { get; set; }
+    }
+}
