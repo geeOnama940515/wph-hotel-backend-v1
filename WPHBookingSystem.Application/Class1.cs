a208e0459489d4564b383abda8a2ced8fa1d67d0
Gregorio Amano III <66071367+geeOnama940515@users.noreply.github.com>
2025-06-09 15:20:29 +0800

    Added DTOs

--- WPHBookingSystem.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WPHBookingSystem.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ WPHBookingSystem.Application/DTOs/Booking/BookingDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPHBookingSystem.Domain.Enums;
+
+namespace WPHBookingSystem.Application.DTOs.Booking
+{
+    public class BookingDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid RoomId { get; set; }
+
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public int Guests { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public BookingStatus Status { get; set; }
+
+        public string SpecialRequests { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+        public string? RoomName { get; set; } // optional projection
+    }
+}
