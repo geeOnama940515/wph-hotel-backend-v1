65c043ed617158839a3476c1bc9fa0bb4d5e52a1
Gregorio Amano III <gregamano@ilocosscript.com>
2025-06-14 16:32:11 +0800

    minimal update

---
+++ WPHBookingSystem.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPHBookingSystem.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string Fullname
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.Firstname, this.LastName);
+            }
+        }
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
---
+++ WPHBookingSystem.Infrastructure/Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPHBookingSystem.Application.Interfaces.Common;
+using WPHBookingSystem.Domain.Entities;
+using WPHBookingSystem.Infrastructure.Identity;
+
+namespace WPHBookingSystem.Infrastructure.Persistence.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Room> Rooms => Set<Room>();
+
+        public DbSet<Booking> Bookings => Set<Booking>();
+    }
+}
