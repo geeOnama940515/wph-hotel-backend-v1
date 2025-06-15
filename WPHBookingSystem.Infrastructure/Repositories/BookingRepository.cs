2c7f28b50fabe25766d88c7f77dfacdd1ab2f78a
Gregorio Amano III <gregamano@ilocosscript.com>
2025-06-15 10:12:58 +0800

    Added basic repositories

---
+++ WPHBookingSystem.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPHBookingSystem.Application.Interfaces;
+using WPHBookingSystem.Domain.Entities;
+using WPHBookingSystem.Infrastructure.Persistence.Data;
+
+namespace WPHBookingSystem.Infrastructure.Repositories
+{
+    public class BookingRepository(ApplicationDbContext context) : GenericRepository<Booking>(context), IBookingRepository
+    {
+        private readonly ApplicationDbContext _context = context;  
+        public Task<List<Booking>> GetBookingsByRoomIdAsync(Guid roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Booking>> GetBookingsByUserIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetOccupancyRateAsync(Guid roomId, DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<decimal> GetTotalRevenueAsync(Guid roomId, DateTime? start = null, DateTime? end = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
