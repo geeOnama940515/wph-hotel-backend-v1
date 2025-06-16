0c965fa8393aef9806329071aa6f517940700955
Gregorio Amano III <gregamano@ilocosscript.com>
2025-06-16 20:28:45 +0800

    added facade

--- WPHBookingSystem.Application/ApplicationDependencyInjection.cs
+++ WPHBookingSystem.Application/ApplicationDependencyInjection.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPHBookingSystem.Application.Interfaces.Services;
+using WPHBookingSystem.Application.Services;
+using WPHBookingSystem.Application.UseCases.Bookings;
+using WPHBookingSystem.Application.UseCases.Rooms;
 
 namespace WPHBookingSystem.Application
 {
@@ -11,6 +15,26 @@ namespace WPHBookingSystem.Application
     {
         public static IServiceCollection ApplicationDependencyiInjection(this IServiceCollection services)
         {
+            // Register Use Cases
+            services.AddScoped<CreateBookingUseCase>();
+            services.AddScoped<UpdateBookingUseCase>();
+            services.AddScoped<UpdateBookingStatusUseCase>();
+            services.AddScoped<CancelBookingUseCase>();
+            services.AddScoped<GetUserBookingsUseCase>();
+
+            services.AddScoped<CreateRoomUseCase>();
+            services.AddScoped<UpdateRoomUseCase>();
+            services.AddScoped<UpdateRoomStatusUseCase>();
+            services.AddScoped<GetRoomByIdUseCase>();
+            services.AddScoped<GetAllRoomsUseCase>();
+            services.AddScoped<DeleteRoomUseCase>();
+            services.AddScoped<CheckRoomAvailabilityUseCase>();
+            services.AddScoped<GetRoomOccupancyRateUseCase>();
+            services.AddScoped<GetRoomRevenueUseCase>();
+
+            // Register Facade
+            services.AddScoped<IBookingSystemFacade, BookingSystemFacade>();
+
             return services;
         }
     }
